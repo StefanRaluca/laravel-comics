0a37dac5202033885561cd7f5797236951ccf7a6
StefanRaluca <raluca.bubulina@yahoo.com>
2024-05-07 14:40:56 +0200

    add layouts and partials

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit necessitatibus autem magnam accusantium, voluptatibus
+        natus architecto ea fugiat dicta laudantium ullam magni omnis dignissimos excepturi voluptatem iure recusandae vitae
+        qui?</h1>
+@endsection
